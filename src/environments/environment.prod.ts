5a2a29f96c73bfaae93e41883b9819358b6119fa
Roberto García <roberto@rhizomik.net>
2019-04-10 12:22:18 +0200

    Prepare webmemory client

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  API: 'https://webingo-gei-api.herokuapp.com'
+  API: 'https://webmemory-api.herokuapp.com'
 };
--- src/karma.conf.js
+++ src/karma.conf.js
@@ -16,7 +16,7 @@ module.exports = function (config) {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
     coverageIstanbulReporter: {
-      dir: require('path').join(__dirname, '../coverage/webingo-gei'),
+      dir: require('path').join(__dirname, '../coverage/webmemory'),
       reports: ['html', 'lcovonly', 'text-summary'],
       fixWebpackSourcePaths: true
     },
